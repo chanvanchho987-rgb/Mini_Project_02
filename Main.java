c06d8842549de04e75edde3f851d077604540cc7
sva <chanvanchho987@gmail.com>
2026-06-11 16:19:51 +0700

    u

--- Main.java
+++ Main.java
@@ -1,3 +1,26 @@
-public class Main {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("View.fxml"));
+
+        //
+        Scene scene = new Scene(root);
+
+        //
+        stage.setScene(scene);
+        //
+        stage.show();
+    }
 }
